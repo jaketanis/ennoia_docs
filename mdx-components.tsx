29227b07c5ce4acf6f5f3866692f9b8bcf1927f8
izzy <jakktan@gmail.com>
2025-08-27 22:19:31 -0500

    Integrated the openapi specs and made them viewable in the dev server.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,13 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from 'fumadocs-ui/mdx'; // basic mdx tools
+import type { MDXComponents } from 'mdx/types'; // ?
+import { APIPage } from 'fumadocs-openapi/ui'; // api doc tools
+import { openapi } from '@/lib/openapi'; // api data
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponents,
-    ...components,
+    ...defaultMdxComponents, // gets standard components
+    APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />, //displays interactive API documentation
+    ...components, // for additional components
   };
 }
